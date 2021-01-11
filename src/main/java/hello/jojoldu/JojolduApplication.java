ea8489525196d8f02a2df55a98050d2612f30e77
ppotto <ppotto.ch@gmail.com>
2021-01-11 19:52:11 +0900

    Initial commit

---
+++ src/main/java/hello/jojoldu/JojolduApplication.java
@@ -0,0 +1,13 @@
+package hello.jojoldu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojolduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JojolduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/jojoldu/JojolduApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.jojoldu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojolduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
