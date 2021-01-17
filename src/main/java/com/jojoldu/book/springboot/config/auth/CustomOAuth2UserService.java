28ab1169ef2accf4f93de6db559fde7dc7a8bc09
ppotto <ppotto.ch@gmail.com>
2021-01-18 03:30:14 +0900

    CustomOAuth2UserService 작성 중

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -9,5 +9,7 @@ public class CustomOAuth2UserService implements OAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         return null;
+
+
     }
 }
