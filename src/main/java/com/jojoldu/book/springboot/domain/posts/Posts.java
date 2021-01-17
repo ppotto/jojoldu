2ee663e9d29c50266d36de56c757031e3830c5c3
vivi-github <vivi-github@users.noreply.github.com>
2021-01-18 01:48:08 +0900

    solved static resource path problem

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,29 +10,29 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 500, nullable = false)
     private String title;
 
-    @Column(columnDefinition =  "TEXT", nullable = false)
-    private  String content;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 
     private String author;
 
     @Builder
-    public Posts(String title, String content, String author){
+    public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
     }
 
-    public void update(String title, String content){
-        this.title =title;
+    public void update(String title, String content) {
+        this.title = title;
         this.content = content;
     }
 
